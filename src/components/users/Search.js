cc526216f6c9598d352f3b0935c04b1ee214b8a0
MischaGithub <mischa.achmat120212@gmail.com>
2020-06-09 19:35:35 -1100

    Just completed the useState and useEffect Hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,64 +1,58 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-export class Search extends Component {
-  state = {
-    text: "",
-  };
+// Converted a  Search class to a functional component and using the useState Hook
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+  const [text, setText] = useState("");
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
+  // On Submit method
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      setAlert("Please enter a name or username", "light");
+    } else {
+      searchUsers(text);
+      setText("");
+    }
   };
 
   // Allowing the search bar to take input
   // With an event parameter
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
-  // On Submit method
-  onSubmit = (e) => {
-    e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert("Please enter a name or username", "light");
-    } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
-    }
-  };
+  const onChange = (e) => setText(e.target.value);
 
-  render() {
-    // Setting the showClear and clearUsers to props
-    // So we can just run showClear and clearUsers
-    const { showClear, clearUsers } = this.props;
+  return (
+    // Creating a Search bar for the users to be serach based on their name
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClear && (
+        // THE CLEAR BUTTON
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
-    return (
-      // Creating a Search bar for the users to be serach based on their name
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input
-            type="text"
-            name="text"
-            placeholder="Search Users..."
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {showClear && (
-          // THE CLEAR BUTTON
-          <button className="btn btn-light btn-block" onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
 
 export default Search;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,116 +1,113 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, useEffect } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export class User extends Component {
-  //
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
+// Converted User class to a functional component and using useEffect Hook
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    //eslint-disable-next-line
+  }, []);
 
-  // PropTypes
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-  };
-  render() {
-    // Pulling all the data from Github
-    const {
-      name,
-      company,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hierable,
-    } = this.props.user;
+  // Pulling all the data from Github
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hierable,
+  } = user;
 
-    const { loading, repos } = this.props;
+  // If Loading then return a Spinner
+  if (loading) return <Spinner />;
+  return (
+    // User Component UI & layout
+    // So we have the layout according to what the desired outcome should be
+    <Fragment>
+      <Link to="/" className="btn btn-light">
+        Back to search
+      </Link>
+      Hireable:{" "}
+      {hierable ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
+      <div className="card grid-2">
+        <div className="all-centre">
+          <img
+            src={avatar_url}
+            className="round-img"
+            alt=""
+            style={{ width: "150px" }}
+          />
 
-    // If Loading then return a Spinner
-    if (loading) return <Spinner />;
-    return (
-      // User Component UI & layout
-      // So we have the layout according to what the desired outcome should be
-      <Fragment>
-        <Link to="/" className="btn btn-light">
-          Back to search
-        </Link>
-        Hireable:{" "}
-        {hierable ? (
-          <i className="fas fa-check text-success" />
-        ) : (
-          <i className="fas fa-times-circle text-danger" />
-        )}
-        <div className="card grid-2">
-          <div className="all-centre">
-            <img
-              src={avatar_url}
-              className="round-img"
-              alt=""
-              style={{ width: "150px" }}
-            />
-
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && (
-              <Fragment>
-                <h3>Bio</h3>
-                <p>{bio}</p>
-              </Fragment>
-            )}
-            <a href={html_url} className="btn btn-dark my-1">
-              View Github Profile
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <Fragment>
-                    <strong>Username:</strong> {login}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {company && (
-                  <Fragment>
-                    <strong>Company:</strong> {company}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <Fragment>
-                    <strong>Website:</strong> {blog}
-                  </Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
         </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">Followers: {followers}</div>
-          <div className="badge badge-success">Following: {following}</div>
-          <div className="badge badge-danger">Public Repos: {public_repos}</div>
-          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            View Github Profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username:</strong> {login}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company:</strong> {company}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website:</strong> {blog}
+                </Fragment>
+              )}
+            </li>
+          </ul>
         </div>
-        <Repos repos={repos} />
-      </Fragment>
-    );
-  }
-}
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-danger">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
+
+User.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired,
+};
 
 export default User;
